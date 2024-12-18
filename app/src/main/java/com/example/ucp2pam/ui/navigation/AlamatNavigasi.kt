594cfd5d019645ca0a0b8ecc3ec0a74723c36670
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-12-18 14:23:43 +0700

    membuat object DestinasiUpdate

--- app/src/main/java/com/example/ucp2pam/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2pam/ui/navigation/AlamatNavigasi.kt
@@ -13,3 +13,9 @@ object DestinasiDetail : AlamatNavigasi{
     const val ID = "id"
 
 }
+
+object DestinasiUpdate : AlamatNavigasi{
+    override val route ="update"
+    const val ID = "id"
+val routesWithArg = "$route/{$ID}"
+}
