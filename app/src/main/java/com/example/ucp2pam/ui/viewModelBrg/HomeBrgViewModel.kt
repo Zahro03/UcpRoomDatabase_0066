c976e7684d56e38b7bd2a182fc901fa071c6b0cc
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-12-18 14:38:54 +0700

    membuat class HomeBrgViewModel.kt

---
+++ app/src/main/java/com/example/ucp2pam/ui/viewModelBrg/HomeBrgViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.ucp2pam.ui.viewModelBrg
+
+import androidx.lifecycle.ViewModel
+import com.example.ucp2pam.data.repository.RepositoryBrg
+
+class HomeBrgViewModel(
+    private val repositoryBrg: RepositoryBrg
+): ViewModel(){
+
+}
