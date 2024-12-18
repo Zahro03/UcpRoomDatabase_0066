dd268ade0aa75c117f5878cd90c9e1bfd529a2de
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-12-18 19:49:28 +0700

    Mengatur tampilan isi halaman berdasarkan status data barang

--- app/src/main/java/com/example/ucp2pam/ui/view/Barang/DetailBrgView.kt
+++ app/src/main/java/com/example/ucp2pam/ui/view/Barang/DetailBrgView.kt
@@ -1,7 +1,16 @@
 package com.example.ucp2pam.ui.view.Barang
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
@@ -10,6 +19,49 @@ fun DetailBrgView(
     viewModel: DetailBrgViewModel = viewModel(factory = PenyediaViewModel.Factory),
     onBack: () -> Unit = { },
     onEditClick: (Int) -> Unit = { },
+    onDeleteClick: () -> Unit = { }
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                judul = "Detail Barang",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    onEditClick(viewModel.detailUiState.value.detailUiEvent.id)
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Barang",
+                )
+            }
+        }
+    ) { innerPadding ->
+        val detailUiState by viewModel.detailUiState.collectAsState()
+
+        BodyDetailBrg(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = detailUiState,
+            onDeleteClick = {
+                viewModel.deleteBarang()
+                onDeleteClick()
+            }
+        )
+    }
+}
+
+@Composable
+fun BodyDetailBrg(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState = DetailUiState(),
     onDeleteClick: () -> Unit = { }
 ){
 
