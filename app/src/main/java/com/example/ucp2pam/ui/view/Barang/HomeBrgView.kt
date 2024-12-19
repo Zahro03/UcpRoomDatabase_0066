012a79b71f91cf90652c52b8910a7d1bf7150de0
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-12-19 14:30:44 +0700

    Menampilkan barang dalam bentuk Card

--- app/src/main/java/com/example/ucp2pam/ui/view/Barang/HomeBrgView.kt
+++ app/src/main/java/com/example/ucp2pam/ui/view/Barang/HomeBrgView.kt
@@ -4,11 +4,13 @@ package com.example.ucp2pam.ui.view.Barang
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -158,5 +160,10 @@ fun CardBarang(
     modifier: Modifier = Modifier,
     onClick: (String) -> Unit = {}
 ){
-
+    Card(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    )
 }
