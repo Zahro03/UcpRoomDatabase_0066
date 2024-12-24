d3cc72ccba5ef0f77d447033f6e54ccd803c795d
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-12-24 11:09:10 +0700

    finish

--- app/src/main/java/com/example/ucp2pam/data/entity/Barang.kt
+++ app/src/main/java/com/example/ucp2pam/data/entity/Barang.kt
@@ -1,4 +1,16 @@
-package com.example.ucp2pam.data.entity
+package com.example.ucp2pam.data.model
 
-class Barang {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import brgUIState
+
+@Entity(tableName = "tblBarang")
+data class Barang(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val namaBarang: String,
+    val deskripsi: String,
+    val harga: Int,
+    val stok: Int,
+    val namaSuplier: String
+)
