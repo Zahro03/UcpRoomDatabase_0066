b86fe83ac38c0c7e09885ec1e5308d4289c16cf6
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-12-24 11:09:34 +0700

    perbaikan typo

--- app/src/main/java/com/example/ucp2pam/data/entity/Suplier.kt
+++ app/src/main/java/com/example/ucp2pam/data/entity/Suplier.kt
@@ -1,4 +1,12 @@
-package com.example.ucp2pam.data.entity
 
-class Suplier {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tblSupplier")
+data class Suplier(
+    @PrimaryKey (autoGenerate = true)
+    val id: Int = 0,
+    val nama: String,
+    val kontak: String,
+    val alamat: String
+)
